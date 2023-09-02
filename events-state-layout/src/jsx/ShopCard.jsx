ffc6c9777596cc91ddfec5b43d21031000078411
Antonio-87 <davydenko.87@list.ru>
2023-09-02 15:17:25 +0300

    css

--- events-state-layout/src/jsx/ShopCard.jsx
+++ events-state-layout/src/jsx/ShopCard.jsx
@@ -6,7 +6,7 @@ const ShopCard = (props)  => {
             <h1>{name}</h1>
             <p>{color}</p>
             <img src={img} alt={name} />
-            <div className="add-cart">
+            <div className="addCart">
                 <span>{"$" + price}</span>
                 <button>ADD TO CART</button>
             </div>
